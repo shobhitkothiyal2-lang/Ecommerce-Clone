d71277529a7a8e189f925dfe00967c877fcab06f
shobhitkothiyal2-lang <shobhitkothiyal2@gmail.com>
2026-01-30 13:55:09 +0530

    xyz

--- FrontEnd/src/Redux/store.js
+++ FrontEnd/src/Redux/store.js
@@ -2,11 +2,10 @@ import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { thunk } from "redux-thunk";
 import { authReducer } from "./Auth/reducer";
 import { customerProductReducer } from "./Customers/Product/reducer";
-
-import { cartReducer } from "./Customers/Cart/Reducer.js";
 import { orderReducer } from "./Customers/Order/Reducer.js";
 import { couponReducer } from "./Customers/Coupon/Reducer.js";
 import { reviewReducer } from "./Customers/Review/Reducer.js";
+import { cartReducer} from "./Customers/Cart/Reducer.js"
 
 const rootReducer = combineReducers({
   auth: authReducer,
